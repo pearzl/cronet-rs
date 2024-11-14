f6f0e21863f893193b5618cfc7dcc309bb536aa9
pearzl <pearzl>
2024-11-14 20:52:51 +0800

    opt enum name

--- build.rs
+++ build.rs
@@ -13,6 +13,8 @@ fn bindgen_ffi() {
     let bindings = builder()
         .headers(["stdbool.h", "include/cronet.idl_c.h"])
         .allowlist_item(CRONET_PREFIX)
+        .parse_callbacks(Box::new(BindgenParseCallback))
+        .rustified_non_exhaustive_enum(CRONET_PREFIX)
         .prepend_enum_name(false)
         .generate()
         .unwrap();
@@ -22,4 +24,24 @@ fn bindgen_ffi() {
     binding_file.push("bindings.rs");
 
     bindings.write_to_file(binding_file).unwrap();
+}
+
+#[derive(Debug)]
+struct BindgenParseCallback;
+
+impl bindgen::callbacks::ParseCallbacks for BindgenParseCallback {
+    fn header_file(&self, filename: &str) {
+        println!("cargo:rerun-if-changed={}", filename);
+    }
+
+    fn enum_variant_name(
+        &self,
+        enum_name: Option<&str>,
+        original_variant_name: &str,
+        _variant_value: bindgen::callbacks::EnumVariantValue,
+    ) -> Option<String> {
+        let prefix = &enum_name?[5..]; // "trim starting `enum `"
+        let new_name = original_variant_name.trim_start_matches(prefix);
+        Some(new_name[1..].to_string())
+    }
 }
--- include/cronet.idl_c.h
+++ include/cronet.idl_c.h
@@ -15,7 +15,6 @@ extern "C" {
 #include <stdint.h>
 
 typedef const char* Cronet_String;
-typedef const char* Cronet_TestType;
 typedef void* Cronet_RawDataPtr;
 typedef void* Cronet_ClientContext;
 
